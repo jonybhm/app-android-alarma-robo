a01043717db1a7c552d26cf95c1f832bcb9a81c7
unknown <jony.bhm@gmail.com>
2025-05-01 22:42:00 -0300

    added visual

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.alarmaRobo',
-  appName: 'app-alarma-robo',
+  appName: 'Alarma Robo',
   webDir: 'www'
 };
 
